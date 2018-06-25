71935848d76bc1d65e0195ba31f2a736d8f09526
Dan <dan@Dans-MacBook-Pro.local>
2018-06-25 22:37:11 +0100

    add interconnector data and polyline

---
+++ public/js/interconn-data.js
@@ -0,0 +1,12 @@
+var interConnData = {
+    "type": "FeatureCollection",
+    "features": [
+    { "type": "Feature", "properties": { "Operator (or Applicant)": "Britned", "Site Name": "Grain Static Inverter", "Technology Type": "Interconnector", "Installed Capacity (MWelec)": "1000.00", "Operational": "2009", "Region": "Southeast", "Development Status (short)": "Operational" }, "geometry": { "type": "Point", "coordinates": [ 0.716667, 51.44] } },
+    { "type": "Feature", "properties": { "Operator (or Applicant)": "Britned", "Site Name": "Maasvlakte Static Inverter", "Technology Type": "Interconnector", "Installed Capacity (MWelec)": "1000.00", "Operational": "2009", "Region": "Holland", "Development Status (short)": "Operational" }, "geometry": { "type": "Point", "coordinates": [ 4.021389, 51.9575] } }
+    ]
+}  
+
+var latlngs = [
+    [0.716667, 51.44],
+    [4.021389, 51.9575]
+]
