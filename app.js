09124a748f89269781d0976ff45f1e053be3e1ec
Dan Thory <Daniel.Thory@leics-his.nhs.uk>
2020-04-28 22:47:50 +0100

    update to add fifth connector and new calculation for embedded wind

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 require('dotenv').config();
 
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
 //to go above the route handlers
 // Body Parser middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // View Engine setup
 app.set('view engine', 'ejs');
 // Static folder
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 app.use(require('./routes'));
 
 // any other route catch-all
-app.get('*', function(req, res) {
-    res.send("Sorry page not found..");
+app.get('*', function (req, res) {
+  res.send('Sorry page not found..');
 });
 
 //===========================
-var port = process.env.PORT || 3000
+var port = process.env.PORT || 3500;
 
-app.listen(port, function() {
-    console.log("Energy Dashboard Server has started");
+app.listen(port, function () {
+  console.log('Energy Dashboard Server has started');
 });
